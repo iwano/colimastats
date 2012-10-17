e28661fc8e37291bdfed3ce3ec73c57686a6a1ce
Ivan Calderon <iwano@hotmail.com>
2012-10-17 15:55:55 -0500

    First commit

---
+++ app/models/location.rb
@@ -0,0 +1,14 @@
+class Location < ActiveRecord::Base
+ attr_accessible :address, :latitude, :longitude, :name
+ acts_as_gmappable :latitude => 'latitude', 
+                    :longitude => 'longitude', 
+                    :check_process => :prevent_geocoding,
+                    :process_geocoding => true
+
+      def gmaps4rails_address
+          address
+      end
+       def gmaps4rails_infowindow
+         "<h4>#{name}</h4>" << "<h4>#{address}</h4>"
+     end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Colimastats::Application.config.secret_token = '014bf37275c24f4755ecfb6ec86d36d66237a24b3783d25ac29aeca8eccd2d7b95bae23298020e43a4f45a9ce1f016a54b6ef70cebd53b0b441df3aa39ec0ce1'
